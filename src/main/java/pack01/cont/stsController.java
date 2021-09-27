abf31f95b4d8ebeba2d2866f2b52c05a5d473096
taehoon95 <leeth950304@gmail.com>
2021-09-27 13:49:51 +0900

    springboot

---
+++ src/main/java/pack01/cont/stsController.java
@@ -0,0 +1,20 @@
+package pack01.cont;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class stsController {
+	@RequestMapping("/")
+	public String main() {
+		System.out.println("index");
+		return "index";
+	}
+	@RequestMapping("/tiger")
+	public String tiger() {
+		System.out.println("tiger");
+		return "tiger";
+	}
+}
+
